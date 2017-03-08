7f846038f7c5b732755849c74a557d2807994fd2
Suneeta Karri <suneetak@mit.edu>
2017-03-08 10:55:01 -0500

    class ex

--- suneetakarri/exercise.js
+++ suneetakarri/exercise.js
@@ -15,13 +15,19 @@ $('h3').each(function(i,element){
 
 // get courses using a regular expression
 exercise.getCourses = function(){
-    return 'Error: getCourses function not implemented';
+	var expression = /<h3>(.*?)<br><I>/g;
+	var matches = courses.match(expression);
+	// console.log(matches);
+    return matches;
 };
 
 // get title word arrays, filter out punctuation/numbers
 // use map
 exercise.getWords = function(titles){
-    return 'Error: getWords function not implemented';
+	var words = titles.map(function(title){
+	return title.toLowerCase().match(/([a-z]+)/g);
+});
+	return words;
 };
 
 // flatten the 2D words array using reduce
